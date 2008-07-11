a9206d6d3110b191133f99ae30d17035c9585454
Ryan Bates <ryan@railscasts.com>
2008-07-11 10:55:53 -0700

    adding freeze and unfreeze association methods when enabling freezing

--- lib/association_freezer/model_additions.rb
+++ lib/association_freezer/model_additions.rb
@@ -1,6 +1,15 @@
 module AssociationFreezer
   module ModelAdditions
     def enable_association_freezer
+      reflect_on_all_associations(:belongs_to).each do |association|
+        define_method "freeze_#{association.name}" do
+          
+        end
+        
+        define_method "unfreeze_#{association.name}" do
+          
+        end
+      end
     end
   end
 end
--- spec/association_freezer/model_additions_spec.rb
+++ spec/association_freezer/model_additions_spec.rb
@@ -4,4 +4,14 @@ describe AssociationFreezer::ModelAdditions do
   it "should add enable_association_freezer method to model class" do
     Order.should respond_to(:enable_association_freezer)
   end
+  
+  it "should add a freeze association method when enabling freezing" do
+    Order.enable_association_freezer
+    Order.new.should respond_to(:freeze_ship_method)
+  end
+  
+  it "should add an unfreeze association method when enabling freezing" do
+    Order.enable_association_freezer
+    Order.new.should respond_to(:unfreeze_ship_method)
+  end
 end
