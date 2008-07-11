b16764ac059a21301a28173acd431eead5bd648c
Ryan Bates <ryan@railscasts.com>
2008-07-11 13:17:05 -0700

    overriding association accessor method so we can refreeze it when fetched

--- lib/association_freezer/model_additions.rb
+++ lib/association_freezer/model_additions.rb
@@ -2,13 +2,28 @@ module AssociationFreezer
   module ModelAdditions
     def enable_association_freezer
       reflect_on_all_associations(:belongs_to).each do |association|
-        if column_names.include? "frozen_#{association.name}"
-          define_method "freeze_#{association.name}" do
-          
-          end
         
-          define_method "unfreeze_#{association.name}" do
+        # it is very important that we first make sure this hasn't already been done
+        # because otherwise it will result in an endless loop the way alias method works.
+        unless instance_methods.include? "freeze_#{association.name}"
           
+          if column_names.include? "frozen_#{association.name}"
+            define_method "freeze_#{association.name}" do
+              send(association.name, true)
+            end
+        
+            define_method "unfreeze_#{association.name}" do
+              
+            end
+          
+          
+            define_method "#{association.name}_with_frozen_check" do |*args|
+              if !instance_variable_defined?("@#{association.name}") || args.first # force reload
+                send("#{association.name}_without_frozen_check", *args).freeze
+              end
+              instance_variable_get("@#{association.name}")
+            end
+            alias_method_chain association.name, :frozen_check
           end
         end
       end
--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -8,19 +8,40 @@ describe Order do
   describe "with freezer" do
     before(:each) do
       Order.enable_association_freezer
+      @order = Order.new
     end
     
     it "should add a freeze association method when enabling freezing" do
-      Order.new.should respond_to(:freeze_ship_method)
+      @order.should respond_to(:freeze_ship_method)
     end
   
     it "should add an unfreeze association method when enabling freezing" do
-      Order.new.should respond_to(:unfreeze_ship_method)
+      @order.should respond_to(:unfreeze_ship_method)
     end
   
     it "should not add freeze/unfreeze methods to cart association since it doesn't have a frozen column" do
-      Order.new.should_not respond_to(:freeze_cart)
-      Order.new.should_not respond_to(:unfreeze_cart)
+      @order.should_not respond_to(:freeze_cart)
+      @order.should_not respond_to(:unfreeze_cart)
+    end
+    
+    describe "when freezing association" do
+      before(:each) do
+        @ship_method = ShipMethod.create!(:price => 5)
+        @order.ship_method = @ship_method
+        @order.freeze_ship_method
+      end
+      
+      it "should freeze associated model" do
+        @order.ship_method.should be_frozen
+      end
+      
+      it "should not freeze original object" do
+        @ship_method.should_not be_frozen
+      end
+      
+      it "should still consider model frozen after reload" do
+        @order.ship_method(true).should be_frozen
+      end
     end
   end
 end
